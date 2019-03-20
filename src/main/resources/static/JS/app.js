0dee190253d451ff864671f9be6a04c4d74fa0fd
tulasi <tulasipinjala.com>
2019-03-20 14:33:10 -0400

    chages in java script

--- src/main/resources/static/JS/app.js
+++ src/main/resources/static/JS/app.js
@@ -1,12 +1,12 @@
-const app = document.querySelector("#app");
+// const app = document.querySelector("#app");
 
 
-fetch('/songs')
-	.then(response => response.json())
-	.then(songs => {
-		let content = `<ul>`
-		songs.forEach((song) => {
-			content += `<li>${song.songTitle} ${song.duration} ${song.link}</li>`;
+// fetch('/songs')
+// 	.then(response => response.json())
+// 	.then(songs => {
+// 		let content = `<ul>`
+// 		songs.forEach((song) => {
+// 			content += `<li>${song.songTitle} ${song.duration} ${song.link}</li>`;
 // 			<ul> ${songs.comments.map(songs => {return `
 // 				<li> ${song.songTitle} </li> `;
 // 				})
@@ -14,43 +14,43 @@ fetch('/songs')
 // 				</ul>
 //     		songs[0].comments.forEach(comment => {
     		// commentsContent += `<li>${comment.content}</li>`
-		})
-		content += `</ul>`;
-		app.innerHTML = content;
-	})
-	.catch(err => console.log(err));
-
-
-	// fetch("/songs")
-	// .then(response => response.json())
-	// .then(songs => {
-	//   let content = `<ul>`;
-	//   songs.forEach(song => {
-	// 	content += `
-	// 			   <li>
-	// 				   <h3>${song.songTitle}</h3>
-	// 				   <ul>
-	// 					   ${songs.comments
-	// 			.map(comment => {
-	// 			  return `
-	// 							  <li class="book">
-	// 								  <p class="book__description">${comment.content}</p>
-	// 							  </li>
-	// 						  `;
-	// 			})
-	// 			.join("")}
-	// 				   </ul>
-	// 			   </li>
-	// 		   `;
-	//   });
-	//   content += `</ul>`;
-	//   let commentsContent = '<ul>';
-	//   authors[0].comments.forEach(comment => {
-	// 	  commentsContent += `<li>${comment.content}</li>`
-	//   })
-	//   commentsContent += '</ul>'
-	//   app.innerHTML = commentsContent;
+	// 	})
+	// 	content += `</ul>`;
+	// 	app.innerHTML = content;
 	// })
 	// .catch(err => console.log(err));
+
+
+	fetch("/songs")
+	.then(response => response.json())
+	.then(songs => {
+	  let content = `<ul>`;
+	  songs.forEach(song => {
+		content += `
+				   <li>
+					   <h3>${song.songTitle}</h3>
+					   <ul>
+						   ${songs.comment
+				.map(comment => {
+				  return `
+								  <li class="book">
+									  <p class="book__description">${comment.content}</p>
+								  </li>
+							  `;
+				})
+				.join("")}
+					   </ul>
+				   </li>
+			   `;
+	  });
+	  content += `</ul>`;
+	  let commentsContent = '<ul>';
+	  authors[0].comments.forEach(comment => {
+		  commentsContent += `<li>${comment.content}</li>`
+	  })
+	  commentsContent += '</ul>'
+	  app.innerHTML = commentsContent;
+	})
+	.catch(err => console.log(err));
     
 
