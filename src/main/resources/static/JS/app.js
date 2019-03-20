da83584205d0cde45972e9d9015f7468d4dcedb4
Nick Miner <nick.miner18@gmail.com>
2019-03-20 14:30:12 -0400

    displayed song comments in json

--- src/main/resources/static/JS/app.js
+++ src/main/resources/static/JS/app.js
@@ -1,56 +1,56 @@
-const app = document.querySelector("#app");
+// const app = document.querySelector("#app");
 
 
-fetch('/songs')
+// fetch('/songs')
+// 	.then(response => response.json())
+// 	.then(songs => {
+// 		let content = `<ul>`
+// 		songs.forEach((song) => {
+// 			content += `<li>${song.songTitle} ${song.duration} ${song.link}</li>`;
+// // 			<ul> ${songs.comments.map(songs => {return `
+// // 				<li> ${song.songTitle} </li> `;
+// // 				})
+// // 				}.join("")}
+// // 				</ul>
+// //     		songs[0].comments.forEach(comment => {
+//     		// commentsContent += `<li>${comment.content}</li>`
+// 		})
+// 		content += `</ul>`;
+// 		app.innerHTML = content;
+// 	})
+// 	.catch(err => console.log(err));
+
+
+	fetch("/songs")
 	.then(response => response.json())
 	.then(songs => {
-		let content = `<ul>`
-		songs.forEach((song) => {
-			content += `<li>${song.songTitle} ${song.duration} ${song.link}</li>`;
-// 			<ul> ${songs.comments.map(songs => {return `
-// 				<li> ${song.songTitle} </li> `;
-// 				})
-// 				}.join("")}
-// 				</ul>
-//     		songs[0].comments.forEach(comment => {
-    		// commentsContent += `<li>${comment.content}</li>`
-		})
-		content += `</ul>`;
-		app.innerHTML = content;
+	  let content = `<ul>`; 
+	  console.log(songs);
+	  songs.forEach(song => {
+		content += `
+				   <li>
+					   <h3>${song.songTitle}</h3>
+					   <ul>
+						   ${song.comments.map(comment => {
+				  return `
+								  <li>
+									  <p>${comment.commentContent}</p>
+								  </li>
+							  `;
+				})
+				.join("")}
+					   </ul>
+				   </li>
+			   `;
+	  });
+	  content += `</ul>`;
+	  let commentsContent = '<ul>';
+	  songs[0].comments.forEach(comment => {
+		  commentsContent += `<li>${comment.commentContent}</li>`
+	  })
+	  commentsContent += '</ul>'
+	  app.innerHTML = commentsContent;
 	})
 	.catch(err => console.log(err));
-
-
-	// fetch("/songs")
-	// .then(response => response.json())
-	// .then(songs => {
-	//   let content = `<ul>`;
-	//   songs.forEach(song => {
-	// 	content += `
-	// 			   <li>
-	// 				   <h3>${song.songTitle}</h3>
-	// 				   <ul>
-	// 					   ${songs.comments
-	// 			.map(comment => {
-	// 			  return `
-	// 							  <li class="book">
-	// 								  <p class="book__description">${comment.content}</p>
-	// 							  </li>
-	// 						  `;
-	// 			})
-	// 			.join("")}
-	// 				   </ul>
-	// 			   </li>
-	// 		   `;
-	//   });
-	//   content += `</ul>`;
-	//   let commentsContent = '<ul>';
-	//   authors[0].comments.forEach(comment => {
-	// 	  commentsContent += `<li>${comment.content}</li>`
-	//   })
-	//   commentsContent += '</ul>'
-	//   app.innerHTML = commentsContent;
-	// })
-	// .catch(err => console.log(err));
     
 
