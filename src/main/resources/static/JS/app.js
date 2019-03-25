1d1e66322a28e9a7cc7ecdbc1ab0d7c634901961
Ariel Webb <arielwebb12@gmail.com>
2019-03-25 13:17:11 -0400

    nav bar

--- src/main/resources/static/JS/app.js
+++ src/main/resources/static/JS/app.js
@@ -8,17 +8,42 @@ import Artists from './components/Artists'
 main()
 
 function main() {
-	api.getRequest('/songs', songs => {
-		getAppContext().innerHTML = Songs(songs)
-	})
-	api.getRequest('/albums', albums => {
-		getAppContext().innerHTML += Albums(albums)
-	})
 	api.getRequest('/artists', artists => {
-		getAppContext().innerHTML += Artists(artists)
+		getAppContext().innerHTML = Artists(artists)
+	})
+	const albumButton = document.querySelector('.nav__albums');
+	console.log(albumButton);
+	navSongs()
+	navAlbums()
+	navArtists() 
+}
+
+function navArtists() {
+	const artistButton = document.querySelector('.nav__artists');
+	events.on(document.querySelector('.nav__artists'), 'click', ()=> {
+		api.getRequest('/artists', artists => {
+			getAppContext().innerHTML = Artists(artists)
+		})
 	})
 }
 
+function navAlbums() {
+	const albumButton = document.querySelector('.nav__albums');
+	events.on(albumButton, 'click', ()=> {
+		api.getRequest('/albums', albums => {
+			getAppContext().innerHTML = Albums(albums)
+		})
+	})
+}
+
+function navSongs() {
+	const songButton = document.querySelector('.nav__songs');
+	events.on(songButton, 'click', ()=> {
+		api.getRequest('/songs', songs => {
+			getAppContext().innerHTML = Songs(songs)
+		})
+	})
+}
 
 function getAppContext() {
 	return document.querySelector('#app')
--- src/main/resources/static/JS/components/Albums.js
+++ src/main/resources/static/JS/components/Albums.js
@@ -7,12 +7,6 @@ export default function Albums(albums) {
                 return `
                     <li class="album">
                         <h3 class="album__albumTitle">${album.albumTitle}</h3>
-                        <p class="album__image">${album.image}</p>
-                        <p class="album__recordLabel">${album.recordLabel}</p>
-                        // move list of songs to somewhere else. the rest should stay here on album page
-                        <ul class="songs">
-                            <li class="song">${songs(album.songs)}</li>
-                        </ul>
                     </li>
                     `
                 }).join('')}
